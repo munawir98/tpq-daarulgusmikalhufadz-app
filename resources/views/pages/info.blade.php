8b7843977f4c1cc2202ff0fcb7bc9e50040dbad9
muhammad munawir <santrineumi@gmail.com>
2026-01-09 07:23:42 +0700

    Inject JS theme initialization to sync with app settings

--- resources/views/pages/info.blade.php
+++ resources/views/pages/info.blade.php
@@ -1,4 +1,11 @@
 <!DOCTYPE html>
+<script>
+    if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+    }
+</script>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
