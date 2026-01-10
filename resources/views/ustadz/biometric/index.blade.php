d2c0c211050db8b773c5e0eca7ab9c08f807be29
muhammad munawir <santrineumi@gmail.com>
2026-01-10 11:10:00 +0700

    Add Accept application/json header to biometric fetch

--- resources/views/ustadz/biometric/index.blade.php
+++ resources/views/ustadz/biometric/index.blade.php
@@ -99,6 +99,7 @@
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
+                    'Accept': 'application/json',
                     'X-CSRF-TOKEN': '{{ csrf_token() }}'
                 },
                 body: JSON.stringify({ credential_id: credId })
