3077582c81b7da9def3d3c1bc65fbe85b1b3d6df
muhammad munawir <santrineumi@gmail.com>
2026-01-26 09:44:10 +0700

    feat: constrain ustadz dashboard width for mobile view on desktop

--- resources/views/ustadz/dashboard.blade.php
+++ resources/views/ustadz/dashboard.blade.php
@@ -345,9 +345,9 @@
     </style>
 </head>
 
-<body class="bg-gray-100 dark:bg-gray-900 font-display min-h-screen p-0 block">
+<body class="bg-gray-100 dark:bg-gray-900 font-display flex justify-center items-center min-h-screen p-0 sm:p-4">
     <div
-        class="relative w-full h-[100dvh] bg-background-light dark:bg-background-dark rounded-none overflow-hidden shadow-none flex flex-col">
+        class="relative w-full max-w-[400px] min-h-[100dvh] sm:min-h-0 sm:h-[850px] bg-background-light dark:bg-background-dark rounded-none sm:rounded-[40px] overflow-hidden shadow-none sm:shadow-2xl flex flex-col">
 
         <!-- Header Background - Blue Gradient -->
         <div
