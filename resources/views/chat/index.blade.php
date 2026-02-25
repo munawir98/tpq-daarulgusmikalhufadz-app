c12318481a5982138608033a5c6d3c55a10f3c2a
muhammad munawir <santrineumi@gmail.com>
2026-02-25 15:10:14 +0700

    feat: link Komunitas icon to group chat

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -168,7 +168,7 @@
             </a>
             {{-- Komunitas --}}
             <a class="flex flex-col items-center gap-1 py-1 px-3 text-gray-400 hover:text-gray-600 dark:hover:text-gray-300 transition-all"
-                href="#">
+                href="{{ route('chat.group') }}">
                 <span class="material-symbols-outlined">groups</span>
                 <span class="text-[10px] font-medium">Komunitas</span>
             </a>
