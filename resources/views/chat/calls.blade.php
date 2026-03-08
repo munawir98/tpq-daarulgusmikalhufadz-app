c7b8ff6da3126297d15c7b1c8a95ec5a298469b3
muhammad munawir <santrineumi@gmail.com>
2026-03-08 13:58:19 +0700

    UI: Remove no-pt to restore top padding on chat headers

--- resources/views/chat/calls.blade.php
+++ resources/views/chat/calls.blade.php
@@ -4,8 +4,6 @@
 
 @section('no-px', true)
 @section('no-gap', true)
-@section('no-pt', true)
-
 @section('header')
 <header class="bg-blue-600 px-4 pt-4 pb-4 shadow-lg relative overflow-hidden">
     {{-- Decorative blobs --}}
