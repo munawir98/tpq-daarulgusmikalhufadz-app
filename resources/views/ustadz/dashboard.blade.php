0cc3c1c19a55222f1c44c00a114da0bb4c91f69a
muhammad munawir <santrineumi@gmail.com>
2026-03-11 21:56:34 +0700

    Ubah warna kotak header jadi biru tua

--- resources/views/ustadz/dashboard.blade.php
+++ resources/views/ustadz/dashboard.blade.php
@@ -351,7 +351,7 @@
         class="relative flex h-full min-h-screen w-full max-w-md mx-auto flex-col bg-background-light dark:bg-background-dark shadow-2xl pb-24 rounded-none sm:rounded-2xl">
 
         <!-- Header Background - Solid Blue -->
-        <div class="absolute top-0 left-0 w-full h-[260px] bg-blue-600 z-0 rounded-b-[40px] overflow-hidden">
+        <div class="absolute top-0 left-0 w-full h-[260px] bg-slate-900 z-0 rounded-b-[40px] overflow-hidden">
         </div>
 
         <!-- Scrollable Content -->
