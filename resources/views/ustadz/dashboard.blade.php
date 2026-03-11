ed041296207b4576f46a54ebec562d5e3942b43b
muhammad munawir <santrineumi@gmail.com>
2026-03-11 22:03:04 +0700

    tambah tekstur pada background header ustadz

--- resources/views/ustadz/dashboard.blade.php
+++ resources/views/ustadz/dashboard.blade.php
@@ -350,8 +350,8 @@
     <div
         class="relative flex h-full min-h-screen w-full max-w-md mx-auto flex-col bg-background-light dark:bg-background-dark shadow-2xl pb-24 rounded-none sm:rounded-2xl">
 
-        <!-- Header Background - Solid Blue -->
-        <div class="absolute top-0 left-0 w-full h-[260px] bg-slate-900 z-0 rounded-b-[40px] overflow-hidden">
+        <!-- Header Background - Dark Blue with Texture -->
+        <div class="absolute top-0 left-0 w-full h-[260px] bg-slate-900 islamic-pattern highlight-overlay z-0 rounded-b-[40px] overflow-hidden">
         </div>
 
         <!-- Scrollable Content -->
